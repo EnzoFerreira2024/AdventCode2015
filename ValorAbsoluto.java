f239c925ad86146505b41b5784eef000dc3472ae
EnzoFerreira2024 <77efmn@gmail.com>
2024-07-11 09:53:00 +0100

    Add files via upload

---
+++ ValorAbsoluto.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.valorabsoluto;
+
+/**
+ *
+ * @author PTC_AppDev_Trainee
+ */
+public class ValorAbsoluto {
+
+    public static void main(String[] args) {
+int numero = -10;
+int valorAbsoluto = Math.abs(numero);
+System.out.println(valorAbsoluto); 
+    }
+}
